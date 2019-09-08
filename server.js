9f11f9778730cf1221215406621c73beb53ac47d
Jenny <jenny.le@ucalgary.ca>
2019-09-07 22:29:38 -0600

    Added some star decorations

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const request = require('request');
 const app = express(); // create an express application 
 const port = 3000;
 
-const apiKey = '************************';
+const apiKey = 'b11cb840317809ece24a4b18f188522f';
 
 // Expose css to Express -> access static files within the public folder
 app.use(express.static('public'));
@@ -19,6 +19,8 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('index', {
         weather: req.weather,
+        humidity: req.weather,
+        clothing: ``,
         error: req.error
     });
 });
@@ -30,14 +32,25 @@ app.post('/', function (req, res) {
     
     request(url, function (err, response, body) {
         if(err) { // if there is an error
-            res.render('index', {weather: null, error: 'error occurred. please try again.'});
+            res.render('index', {weather: null, humidity: null, clothing: null, error: 'error occurred. please try again.'});
         } else {
             let weather = JSON.parse(body); // weather contains the JSON object from the request
             if(weather.main == undefined) { // if the city doesn't 'exist'
-                res.render('index', {weather: null, error: 'unknown input. please try again.'});
+                res.render('index', {weather: null, humidity: null, clothing: null, error: 'unknown input. please try again.'});
             } else {
                 let weatherResult = `It is ${weather.main.temp} degrees in ${weather.name}!`; // formulate the outputs
-                res.render('index', {weather: weatherResult, error: null}); // render it
+                let humidityResult = `Humidity: ${weather.main.humidity}%`;
+                
+                let clothingResult = ``;
+                if (`${weather.main.temp}` < 0) {
+                    clothingResult = `Seems pretty cold. You might want to put on a lot of layers.`;
+                } else if ((`${weather.main.temp}` > 0) && (`${weather.main.temp}` <= 17)) {
+                    clothingResult = `Seems kind of chilly. You might want to put on a jacket.`;
+                } else if ((`${weather.main.temp}` > 17)) {
+                    clothingResult = `Seems pretty hot. You should probably wear some shorts.`;
+                }
+
+                res.render('index', {weather: weatherResult, humidity: humidityResult, clothing: clothingResult, error: null}); // render it
             }
         }
     });
